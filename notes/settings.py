8bfd46fe7bfaeeba0c6a31021f0bcfd9d1e98728
Raghav Pawar <raghavdoon@gmail.com>
2021-08-19 21:44:20 +0530

    Static settings updated

--- notes/settings.py
+++ notes/settings.py
@@ -148,7 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
