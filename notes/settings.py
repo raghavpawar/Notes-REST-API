c0edeb32a9259b90d8258f67cabec6ef6824709d
Raghav Pawar <raghavdoon@gmail.com>
2021-08-20 16:38:32 +0530

    Added Django Heroku

--- notes/settings.py
+++ notes/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -193,3 +193,5 @@ AWS_S3_VERIFY = True
 AWS_S3_REGION_NAME = 'ap-south-1'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
